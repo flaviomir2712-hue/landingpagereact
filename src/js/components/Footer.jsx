65b5488dcdb126558a80717d2aebc71be0480b0c
flaviomir2712-hue <flaviomir2712@gmail.com>
2026-03-04 12:02:32 +0000

    listo

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="card text-center bg-light pt-3">
+            <div className="card-body">
+                <h5 className="card-title">Copyright </h5>
+            </div>
+        </div>
+
+    );
+};
+export default Footer;
